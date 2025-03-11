96958b06f7459db18ab7adbd517438590ebb0e3c
danielfsantoss <danielfigueiredo009@gmail.com>
2025-03-11 16:10:50 -0300

    fix: Error fixed: failed to create symbolic link "/usr/local/bin/timetracker"

--- timetracker.sh
+++ timetracker.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 chmod +x "$DIR/timetracker.py"
 
 # Criar link simbólico para o diretório em /usr/local/bin
-ln -sf "$DIR/timetracker.py" "/usr/local/bin/timetracker"
+sudo ln -sf "$DIR/timetracker.py" "/usr/local/bin/timetracker"
 
 echo "TimeTracker instalado com sucesso!"
 echo "Você pode agora usar 'timetracker' de qualquer diretório."
